834476daec3f3ba6ae3b5fa443c049074d34ade2
Charlie BOYER <charlieboyer@outlook.fr>
2025-05-22 10:44:04 +0200

    fix(xr-interactions): replace IRespawnable by XRObjectBase abstract class
    * Derives XRObjectBase for potential overrides on certain objects

--- Assets/Code/Scripts/Source/XR/XRObjectBase.cs
+++ Assets/Code/Scripts/Source/XR/XRObjectBase.cs
@@ -3,36 +3,33 @@ using UnityEngine;
 namespace Code.Scripts.Source.XR
 {
 
-    public abstract class GenericObject : MonoBehaviour, IRespawnable
+    public abstract class XRObjectBase : MonoBehaviour
     {
-        [SerializeField] private float _resetDelay = 5f;
-        private Vector3 _initialPosition;
-        private Quaternion _initialRotation;
-        private Rigidbody _rigidbody;
+        [SerializeField] protected float _resetDelay = 5f;
+        protected Vector3 _initialPosition;
+        protected Quaternion _initialRotation;
+        protected Rigidbody _rigidbody;
 
-        private void Awake()
+        protected virtual void Init()
         {
             _rigidbody = GetComponent<Rigidbody>();
             _initialPosition = transform.position;
             _initialRotation = transform.rotation;
         }
 
-        public void Respawn()
+        protected virtual void Respawn()
         {
             _rigidbody.angularVelocity = Vector3.one;
             transform.position = _initialPosition;
             transform.rotation = _initialRotation;
         }
-        
-        public void OnTriggerExit(Collider other)
+
+        private void OnTriggerExit(Collider other)
         {
             if (other.CompareTag("GameArea"))
             {
                 Invoke(nameof(Respawn),_resetDelay);
             }
         }
-
-
-      
     }
 }
