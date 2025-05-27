c2de231448a9fb62f3445a2cc974b1e417b6d8c8
Noah MILIANI <120110545+NoahMil@users.noreply.github.com>
2025-05-27 15:30:05 +0200

    integration(narrator) : start narrator script

--- Assets/Code/Scripts/Source/GameFSM/States/GameStateHallIntro.cs
+++ Assets/Code/Scripts/Source/GameFSM/States/GameStateHallIntro.cs
@@ -1,18 +1,22 @@
 using System;
 using Code.Scripts.Source.Audio;
 using Code.Scripts.Source.Managers;
+using Code.Scripts.Source.Narrator;
+using UnityEngine;
 
 namespace Code.Scripts.Source.GameFSM.States
 {
     [Serializable]
     public class GameStateHallIntro : GameBaseState
     {
-       
+        [SerializeField] private VoiceLineSO _voiceLineSO;
+        
         public override void EnterState(GameStateManager context)
         {
             AudioManager.Instance.ChangeRain(AudioManager.Instance.ClipsIndex.RainInDoor);
             AudioManager.Instance.ChangeMusic(AudioManager.Instance.ClipsIndex.GameMusic);
             context.SwitchState(context.GameStates.HallInProgress);
+            Narrator.Narrator.Instance.PlayVoiceLine(_voiceLineSO);
         }
 
         public override void UpdateState(GameStateManager context)
--- Assets/Code/Scripts/Source/Narrator/SubtitleModule.cs
+++ Assets/Code/Scripts/Source/Narrator/SubtitleModule.cs
@@ -1,9 +1,47 @@
 using UnityEngine;
+using TMPro;
+using DG.Tweening;
+using System.Collections;
 
 namespace Code.Scripts.Source.Narrator
 {
-    public class SubtitleModule: MonoBehaviour
+    public class SubtitleModule : MonoBehaviour
     {
-        
+        [SerializeField] private CanvasGroup canvasGroup;
+        [SerializeField] private TextMeshProUGUI subtitleText;
+        [SerializeField] private float fadeDuration = 0.5f;
+        [SerializeField] private float typewriterSpeed = 0.02f;
+
+        private Coroutine subtitleRoutine;
+
+        public void ShowSubtitle(string subtitle, float displayDuration)
+        {
+            if (subtitleRoutine != null)
+                StopCoroutine(subtitleRoutine);
+
+            subtitleRoutine = StartCoroutine(HandleSubtitle(subtitle, displayDuration));
+        }
+
+        private IEnumerator HandleSubtitle(string text, float displayDuration)
+        {
+            subtitleText.text = "";
+            canvasGroup.alpha = 0;
+            canvasGroup.gameObject.SetActive(true);
+
+            canvasGroup.DOFade(1, fadeDuration);
+
+            foreach (char c in text)
+            {
+                subtitleText.text += c;
+                yield return new WaitForSeconds(typewriterSpeed);
+            }
+
+            yield return new WaitForSeconds(displayDuration);
+            canvasGroup.DOFade(0, fadeDuration);
+            yield return new WaitForSeconds(fadeDuration);
+
+            canvasGroup.gameObject.SetActive(false);
+            subtitleRoutine = null;
+        }
     }
 }
