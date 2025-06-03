3fe3657077b4d938d0898f0ce0ba75a4647df4de
NoahMil <120110545+NoahMil@users.noreply.github.com>
2025-06-04 01:27:02 +0200

    integration (sounds + drawers ) : add voice over + fix drawers

---
+++ Assets/Code/Scripts/Source/Audio/DoorSound.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+namespace Code.Scripts.Source.Audio
+{
+    [RequireComponent(typeof(AudioSource), typeof(HingeJoint))]
+    public class DoorSound : MonoBehaviour
+    {
+        [SerializeField] private float openAngleThreshold = 5f;
+        [SerializeField] private float deadZone = 1f;
+        [SerializeField] private bool opensPositively = true;
+
+        private AudioSource _audioSource;
+        private AudioClip _openSound;
+        private AudioClip _closeSound;
+        private HingeJoint _hinge;
+        private bool _isOpen = false;
+
+        private void Start()
+        {
+            _hinge = GetComponent<HingeJoint>();
+            _audioSource = GetComponent<AudioSource>();
+            _openSound = AudioManager.Instance.ClipsIndex.OpenDoor1;
+            _closeSound = AudioManager.Instance.ClipsIndex.CloseDoor1;
+        }
+
+        private void Update()
+        {
+            float angle = _hinge.angle;
+
+            if (opensPositively)
+            {
+                if (!_isOpen && angle > openAngleThreshold + deadZone)
+                {
+                    _isOpen = true;
+                    _audioSource.PlayOneShot(_openSound);
+                }
+                else if (_isOpen && angle < openAngleThreshold - deadZone)
+                {
+                    _isOpen = false;
+                    _audioSource.PlayOneShot(_closeSound);
+                }
+            }
+            else
+            {
+                if (!_isOpen && angle < -openAngleThreshold - deadZone)
+                {
+                    _isOpen = true;
+                    _audioSource.PlayOneShot(_openSound);
+                }
+                else if (_isOpen && angle > -openAngleThreshold + deadZone)
+                {
+                    _isOpen = false;
+                    _audioSource.PlayOneShot(_closeSound);
+                }
+            }
+        }
+    }
+}
--- Assets/Code/Scripts/Source/Audio/DrawerSound.cs
+++ Assets/Code/Scripts/Source/Audio/DrawerSound.cs
@@ -1,70 +1,64 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Code.Scripts.Source.Audio
 {
     [RequireComponent(typeof(AudioSource))]
-    public class DrawerOpenCloseSound : MonoBehaviour
+    public class DrawerMoveSoundSimple : MonoBehaviour
     {
         [SerializeField] private DrawerType _drawerType;
-        [SerializeField] private  float _openThreshold = 0.1f;
-        [SerializeField] private  float _deadZone = 0.005f;
-        private  float _closedPosition;
+        [SerializeField] private float _soundDelay = 2f;
+        [SerializeField] private float _movementThreshold = 0.001f;
+
         private AudioSource _audioSource;
-        private AudioClip _closeSound;
-        private bool _isOpen;
-
-        private AudioClip _openSound;
-
-        void Start()
-        {
-            _audioSource = GetComponent<AudioSource>();
-            switch (_drawerType)
-            {
-                case DrawerType.SmallDrawer:
-                    _openSound = AudioManager.Instance.ClipsIndex.OpenDrawer1;
-                    _closeSound = AudioManager.Instance.ClipsIndex.CloseDrawer1;
-                    break;
-                case DrawerType.MediumDrawer1:
-                    _openSound = AudioManager.Instance.ClipsIndex.OpenDrawer2;
-                    _closeSound = AudioManager.Instance.ClipsIndex.CloseDrawer3;
-                    break;
-                case DrawerType.MediumDrawer2:
-                    _openSound = AudioManager.Instance.ClipsIndex.OpenDrawer3;
-                    _closeSound = AudioManager.Instance.ClipsIndex.CloseDrawer3;
-                    break;
-                case DrawerType.LargeDrawer:
-                    _openSound = AudioManager.Instance.ClipsIndex.OpenDrawer4;
-                    _closeSound = AudioManager.Instance.ClipsIndex.CloseDrawer4;
-                    break;
-                default: break;
-            }
-
-            _closedPosition = transform.localPosition.z;
-        }
-
-        void Update()
-        {
-            float currentZ = transform.localPosition.z;
-            float distance = currentZ - _closedPosition;
-
-            if (!_isOpen && distance > _openThreshold + _deadZone)
-            {
-                _isOpen = true;
-                _audioSource.PlayOneShot(_openSound);
-            }
-            else if (_isOpen && distance < _openThreshold - _deadZone)
-            {
-                _isOpen = false;
-                _audioSource.PlayOneShot(_closeSound);
-            }
-        }
+        private AudioClip _moveSound;
+        private float _lastZ;
+        private float _nextSoundTime;
 
         private enum DrawerType
         {
             SmallDrawer,
             MediumDrawer1,
             MediumDrawer2,
-            LargeDrawer,
+            LargeDrawer
+        }
+
+        private void Start()
+        {
+            _audioSource = GetComponent<AudioSource>();
+            _lastZ = transform.localPosition.z;
+
+            switch (_drawerType)
+            {
+                case DrawerType.SmallDrawer:
+                    _moveSound = AudioManager.Instance.ClipsIndex.OpenDrawer2;
+                    break;
+                case DrawerType.MediumDrawer1:
+                    _moveSound = AudioManager.Instance.ClipsIndex.OpenDrawer1;
+                    break;
+                case DrawerType.MediumDrawer2:
+                    _moveSound = AudioManager.Instance.ClipsIndex.OpenDrawer3;
+                    break;
+                case DrawerType.LargeDrawer:
+                    _moveSound = AudioManager.Instance.ClipsIndex.OpenDrawer4;
+                    break;
+            }
+
+            _nextSoundTime = Time.time;
+        }
+
+        private void Update()
+        {
+            float currentZ = transform.localPosition.z;
+            float movement = Mathf.Abs(currentZ - _lastZ);
+
+            if (movement > _movementThreshold && Time.time >= _nextSoundTime)
+            {
+                _audioSource.PlayOneShot(_moveSound);
+                _nextSoundTime = Time.time + _soundDelay;
+            }
+
+            _lastZ = currentZ;
         }
     }
 }
